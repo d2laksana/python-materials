addf9d2d357778fd1524853aeb5bb0760215a448
Nur Wahid <nurwahid.nw11@gmail.com>
2021-12-14 18:06:59 +0700

    Update pertemuan 10

--- alpro_meet_10/demo_10.py
+++ alpro_meet_10/demo_10.py
@@ -11,9 +11,28 @@ query1 = '''CREATE TABLE IF NOT EXISTS alamat(
     phone_number TEXT);'''
 cursor.execute(query1)
 
+# Insert data
+# query2 = '''INSERT INTO alamat (id, first_name, phone_number) VALUES (1, "John Doe", "0822123")'''
+# cursor.execute(query2)
+# db.commit()
+
+# query3 = '''INSERT INTO alamat (id, first_name, phone_number) VALUES (2, "Andrea Wu", "0812345")'''
+# cursor.execute(query3)
+# db.commit()
+
+# query4 = '''INSERT INTO alamat (id, first_name, phone_number) VALUES (3, "Tylor Cruwe", "0854321")'''
+# cursor.execute(query4)
+# db.commit()
+
+# query5 = '''INSERT INTO alamat (first_name, phone_number) 
+#     VALUES ("Peter Friese", "0855321")'''
+# cursor.execute(query5)
+# db.commit()
+
+
 # Menampilkan data dari database
-query2 = '''SELECT first_name, phone_number FROM alamat'''
-cursor.execute(query2)
+query6 = '''SELECT * FROM alamat'''
+cursor.execute(query6)
 
 for baris in cursor.fetchall():
     print(baris)
